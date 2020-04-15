0c0e6e7a1f710869eeb73db7e6db81ab0eabf813
TuxDave <godinodavide@gmail.com>
2020-04-15 20:23:13 +0200

    About JDatePicker: 	seted up all the limits to prevents the picking of an impossible date.

--- src/com/TuxDave/jcomponents/JDatePicker.java
+++ src/com/TuxDave/jcomponents/JDatePicker.java
@@ -3,9 +3,9 @@ package com.TuxDave.jcomponents;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.logging.ConsoleHandler;
 
 public class JDatePicker extends JPanel {
     private JPanel bodyPanel;
@@ -20,25 +20,28 @@ public class JDatePicker extends JPanel {
     private JSpinner yearSpinner;
     {
         //inizializing all the needed
+        //adding listeners
+        daySpinner.addChangeListener(new JDataPickerListener());
+        yearSpinner.addChangeListener(new JDataPickerListener());
+        monthCombo.addActionListener(new JDataPickerListener());
 
         //creation of a generic model based on String
         monthCombo.setModel(new DefaultComboBoxModel<String>());
-
-        //setting the starting year and the dinamically limits fot the choose of the year
-        Calendar c = Calendar.getInstance();
-        yearSpinner.setValue(c.getTime().getYear()+1900);
-        yearSpinner.addChangeListener(new JDataPickerListener());
-
-        //Adding months to the JComboBox
+        //and add the months
         String[] mesi = new String[]{"January","February","March","April","May","June","July","August","September","October","November","December"};
         for(short i = 0; i < 12; i++){
-            monthCombo.addItem(new String(Integer.toString(i+1) + " " + mesi[1]));
+            monthCombo.addItem(new String(Integer.toString(i+1) + " " + mesi[i]));
         }
+
+        //set the starting year
+        Calendar c = Calendar.getInstance();
+        yearSpinner.setValue(c.getTime().getYear()+1900);
     }
-    private class JDataPickerListener implements ChangeListener{ //creation of the class that implements the listener
+
+    private class JDataPickerListener implements ChangeListener, ActionListener{ //creation of the class that implements the listeners
         @Override
         public void stateChanged(ChangeEvent changeEvent) { //implementation of ChangeListener
-            if(changeEvent.getSource().equals(yearSpinner)){ //if the listener is called by "daySpinner"
+            if(changeEvent.getSource().equals(yearSpinner)){ //if the listener is called by "yearSpinner"
                 Calendar c = Calendar.getInstance();
                 int min = c.getTime().getYear()+1900-125;
                 int now = c.getTime().getYear()+1900;
@@ -48,6 +51,30 @@ public class JDatePicker extends JPanel {
                 if((int)yearSpinner.getValue() > now){
                     yearSpinner.setValue(now);
                 }
+                JDataPickerListener controller = new JDataPickerListener(); //check if the day is ok
+                controller.stateChanged(new ChangeEvent(daySpinner));
+            }
+            if(changeEvent.getSource().equals(daySpinner)){ // condiction about daySpinner
+                int day = (int)(daySpinner.getValue());
+                if(day < 0){
+                    daySpinner.setValue(0);
+                }if(day > 28){
+                    int[] giorni = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+                    int mese = monthCombo.getSelectedIndex();
+                    int anno = (int)(yearSpinner.getValue());
+                    boolean bis = anno % 4 == 0;
+                    if(day > giorni[mese] + (mese == 1 && bis ? 1 : 0)){
+                        daySpinner.setValue(day - 1);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if(actionEvent.getSource().equals(monthCombo)){
+                JDataPickerListener controller = new JDataPickerListener(); //check if the day is ok
+                controller.stateChanged(new ChangeEvent(daySpinner));
             }
         }
     }
