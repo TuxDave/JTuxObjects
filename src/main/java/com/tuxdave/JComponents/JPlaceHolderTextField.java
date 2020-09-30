dd3dda8f7c0a51391bd4d837128c6c28759f7dc5
TuxDave <godinodavide@gmail.com>
2020-09-30 09:36:47 +0200

    now PlaceHolder is better

--- src/main/java/com/tuxdave/JComponents/JPlaceHolderTextField.java
+++ src/main/java/com/tuxdave/JComponents/JPlaceHolderTextField.java
@@ -7,55 +7,24 @@ import java.awt.event.FocusListener;
 
 public class JPlaceHolderTextField extends JTextField{
     public String placeHolder = "";
-    private Color originalColor;
-
-    {
-        addFocusListener(new JPlaceHolderTextFieldListener()); //adds the focus listener
-    }
 
     public void setPlaceHolder(String s){
         placeHolder = s;
-        setPlaceHolderVisible(true);
     }
     public String getPlaceHolder(){
         return placeHolder;
     }
-    public void setPlaceHolderVisible(boolean b){
-        if(b){
-            originalColor = getForeground();
-            setText(placeHolder);
-            setForeground(Color.GRAY);
-        }else{
-            setText("");
-            setForeground(originalColor);
-        }
-    }
 
     @Override
-    public String getText() {
-        if(super.getText().equals(placeHolder)){
-            return "";
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(getFont());
+        g.setColor(Color.GRAY);
+
+        if(getText().equals("")){
+            g.drawString(placeHolder, 5, getHeight()-(getHeight()/2-5));
         }else{
-            return super.getText();
-        }
-    }
-
-    private static class JPlaceHolderTextFieldListener implements FocusListener {
-
-        @Override
-        public void focusGained(FocusEvent focusEvent) {
-            JPlaceHolderTextField self = (JPlaceHolderTextField)focusEvent.getSource();
-            if(self.getText().equals(self.getPlaceHolder()) || self.getText().equals("")){
-                self.setPlaceHolderVisible(false);
-            }
-        }
-
-        @Override
-        public void focusLost(FocusEvent focusEvent) {
-            JPlaceHolderTextField self = (JPlaceHolderTextField)focusEvent.getSource();
-            if(self.getText().equals("")){
-                self.setPlaceHolderVisible(true);
-            }
+            return;
         }
     }
 }
